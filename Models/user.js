d75f1a5768b9932a31da3316d06cf44f98335f6e
TafadzwaG <tafadzwagashirah@gmail.com>
2022-12-09 13:54:33 +0200

    virtual fields and methods on Schmema

--- Models/user.js
+++ Models/user.js
@@ -41,3 +41,36 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+//Virtual Field
+
+userSchema
+  .virtual("password")
+  .set((password) => {
+    this._password = password;
+    this.salt = uuidv4();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(() => {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: (password) => {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User
+
