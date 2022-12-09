1c68e926e613907d311f49714da5a64cebd70905
TafadzwaG <tafadzwagashirah@gmail.com>
2022-12-09 13:15:49 +0200

    database connection

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-
-
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  res.send('Express')
-})
-
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on: ${PORT}`)
-})
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
