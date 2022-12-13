7ee1707145216c7095c4da80b129fd28676e82cb
TafadzwaG <tafadzwagashirah@gmail.com>
2022-12-13 12:39:20 +0200

    category

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,98 @@
 import Category from "../Models/category.js";
 import { errorHandler } from "../helpers/dbErrorHandler.js";
 
+export const categoryById = async (req, res, next) => {
+  try {
+    Category.findById(id).exec((err, category) => {
+      if (err || !category) {
+        return res.status(400).json({
+          error: errorHandler(err) || "There was an error",
+        });
+      }
+
+      req.category = category;
+      next();
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const category = new Category(req.body);
     category.save((err, data) => {
       if (err) {
-
-        console.log(err)
+        console.log(err);
         return res.status(400).json({
-          error: errorHandler(err),
+          error: errorHandler(err) || "There was an error",
         });
       }
 
       res.json({ data });
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const read = async (req, res) => {
+  try {
+    return res.json(req.category);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const list = async (req, res) => {
+  try {
+
+    Category.find().exec((err, data) => {
+      if(err){
+        return res.status(400).json({
+          error: errorHandler(err) || 'Error finding categories'
+        })
+      }
+    })
+  } catch (error) { 
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const category = req.category;
+    category.name = req.body.name;
+
+    category.save((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err) || "Error whilst updating!",
+        });
+      }
+
+      res.json(data);
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const category = req.category;
+
+    category.remove((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err) || "Error whilst removing",
+        });
+      }
+
+      res.json({
+        message: "Category removed",
+      });
     });
   } catch (error) {
     res.status(404).json({ message: error.message });
--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  create } from "../controllers/category.js";
+import {  create, read ,categoryById, update, deleteCategory, list } from "../controllers/category.js";
 import { verifyToken, isAuth, requireSignin } from "../middleware/auth.js";
 import { isAdmin } from "../middleware/admin.js";
 import { userById } from "../controllers/user.js";
@@ -9,8 +9,12 @@ import { userById } from "../controllers/user.js";
 const router = express.Router();
 
 router.param("userId", userById);
+router.param("categoryId", categoryById)
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin ,create)
-
+router.put("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin ,update)
+router.delete("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin ,deleteCategory)
+router.get("/category/:categoryId", read)
+router.get("/categories", list)
 
 
 
